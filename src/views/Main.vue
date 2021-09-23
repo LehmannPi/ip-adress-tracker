49fdb96f5a8dc6aa7e10139b5a965b1000b474ea
Filipe <filipelehmannp@gmail.com>
2021-09-23 11:28:23 -0300

    sketching

---
+++ src/views/Main.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <div class="background">
+			<img src="@/../images/pattern-bg.png" alt="Blue Background Image" class="background__image">
+		</div>
+		<InfoCard />
+    <div class="map"></div>
+  </div>
+</template>
+
+<script>
+import InfoCard from '../components/InfoCard.vue';
+
+export default {
+  components: { InfoCard },
+  name: "Main",
+};
+</script>
+
+<style>
+</style>
