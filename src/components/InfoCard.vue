af3f2bfd6639582dc26d8a6e6bc05360cf57cb27
Filipe <filipelehmannp@gmail.com>
2022-05-15 10:47:03 -0300

    ip-tracker

--- src/components/InfoCard.vue
+++ src/components/InfoCard.vue
@@ -1,8 +1,7 @@
 <template>
-  <c-box d="flex" flex-dir="column" align-items="center">
-    <p class="info__title">IP Address Tracker</p>
-    <!-- <c-input type="text" class="info__inp" /> -->
-    <c-box
+  <CBox d="flex" flex-dir="column" align-items="center">
+    <CText class="info__title">IP Address Tracker</CText>
+    <CBox
       d="flex"
       :width="[
         '88%', // base
@@ -10,12 +9,16 @@
       ]"
       pos="relative"
     >
-      <c-input
+      <CInput
         placeholder="Search for any IP address or domain"
         h="58px"
+        p="24px"
         rounded="12px"
+        fontSize="18px"
+        style="z-index: 3"
+        v-model="ipInput"
       />
-      <c-button
+      <CButton
         icon="chevron-right"
         class="btn_go"
         rounded="12px"
@@ -26,12 +29,13 @@
         right="0"
         bg="black"
         color="white"
+        zIndex="10"
+        @click="apiComm"
       >
         <c-icon fontSize="26px" name="chevron-right" />
-      </c-button>
-    </c-box>
-    <!-- <c-box class="info__tracker"> -->
-    <c-box
+      </CButton>
+    </CBox>
+    <CBox
       d="flex"
       bg="white"
       rounded="12px"
@@ -40,39 +44,33 @@
       zIndex="3"
       class="info-blocks-wrapper"
     >
-      <!-- {{ info }}
-      <p>{{ response }}</p> -->
-
       <div class="info__block">
-        <p class="block__title">ip adress</p>
-        <p class="block__value">b</p>
+        <CText class="block__title">ip adress</CText>
+        <CText class="block__value">{{ this.ipDisplay }}</CText>
       </div>
-			<div class="breakers"></div>
+      <div class="breakers"></div>
       <div class="info__block">
-        <p class="block__title">location</p>
-        <p class="block__value">b</p>
+        <CText class="block__title">location</CText>
+        <CText class="block__value">{{ this.location }}</CText>
       </div>
-			<div class="breakers"></div>
+      <div class="breakers"></div>
       <div class="info__block">
-        <p class="block__title">timezone</p>
-        <p class="block__value">b</p>
+        <CText class="block__title">timezone</CText>
+        <CText class="block__value">{{ this.timezone }}</CText>
       </div>
-			<div class="breakers"></div>
+      <div class="breakers"></div>
       <div class="info__block">
-        <p class="block__title">isp</p>
-        <p class="block__value">b</p>
+        <CText class="block__title">isp</CText>
+        <CText class="block__value">{{
+          this.isp || "Could not determine"
+        }}</CText>
       </div>
-      <!-- <div class="info__ip"></div>
-      <div class="info__location"></div>
-      <div class="info__timezone"></div>
-      <div class="info__isp"></div> -->
-    </c-box>
-  </c-box>
+    </CBox>
+  </CBox>
 </template>
 
 <script>
-import { CInput, CBox, CButton, CIcon } from "@chakra-ui/vue";
-import axios from "axios";
+import { CInput, CBox, CButton, CIcon, CText } from "@chakra-ui/vue";
 
 export default {
   name: "InfoCard",
@@ -81,19 +79,72 @@ export default {
     CBox,
     CButton,
     CIcon,
+    CText,
   },
   data: () => ({
-    // response: null,
-    info: {},
-    response: {},
+    url: `https://ipgeolocation.abstractapi.com/v1/?api_key=${process.env.VUE_APP_ABSTRACT}`,
+    ipInput: "",
+    ipDisplay: "",
+    location: "",
+    timezone: "",
+    isp: "",
+    longitude: 0,
+    latitude: 0,
   }),
+  methods: {
+    locationFormatter: (city, name, postal) => {
+      return city + ", " + name + " " + postal;
+    },
+    regionFormatter: (time) => {
+      if (time.toString().length < 3 && time < 0) {
+        return "GMT -0" + time.toString().slice(1) + ":00";
+      } else if (time.toString().length < 2 && time > 0) {
+        return "GMT 0" + time.toString() + ":00";
+      } else {
+        return "GMT " + time.toString() + ":00";
+      }
+    },
+    async apiComm() {
+      var aux;
+      var flag = false; // ativa para campo nulo
+      const axios = require("axios").default;
+      if (this.ipInput) {
+        aux = this.url + "&ip_address=" + this.ipInput;
+      }
+      if (!this.ipInput) {
+        aux = this.url;
+        flag = true;
+      }
+      console.log(aux);
+      await axios(aux)
+        .then((response) => {
+          console.log(response.data);
+          this.ipDisplay = response.data.ip_address;
+          this.location = response.data.city
+            ? this.locationFormatter(
+                response.data.city,
+                response.data.region_iso_code,
+                response.data.postal_code
+              )
+            : "Could not determine";
+          this.timezone = response.data.timezone.gmt_offset
+            ? this.regionFormatter(response.data.timezone.gmt_offset)
+            : "Could not determine";
+          this.isp = response.data.connection.autonomous_system_organization;
+          this.latitude = response.data.latitude;
+          this.longitude = response.data.longitude;
+        })
+        .catch((error) => {
+          alert("Too many requests, wait a little bit before trying again.");
+          console.log(error);
+        });
+      if (flag) this.ipInput = this.ipDisplay;
+      this.$emit("renderMap", this.latitude, this.longitude, this.location);
+    },
+    async fullProcedure() {},
+  },
   async mounted() {
-    await axios
-      .get("https://api.coindesk.com/v1/bpi/currentprice.json")
-      .then((response) => (this.info = response.data))
-      .catch((error) => console.log(error));
-
-    this.response = this.info.time;
+    await this.apiComm();
   },
 };
 </script>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -7,20 +7,45 @@
         class="background__image"
       />
     </div>
-    <InfoCard />
+    <InfoCard @renderMap="setCoordinates" />
     <div class="map-container">
-      <div id="map"></div>
-			<div class="crap"></div>
+      <l-map id="map" :zoom="zoom" :center="center" :options="mapOptions">
+        <l-tile-layer :url="url" />
+        <l-marker :lat-lng="withPopup">
+          <l-popup>{{ this.location }}</l-popup>
+        </l-marker>
+      </l-map>
     </div>
   </div>
 </template>
 
 <script>
 import InfoCard from "../components/InfoCard.vue";
+import { latLng } from "leaflet";
 
 export default {
   components: { InfoCard },
   name: "Main",
+  data() {
+    return {
+      zoom: 13,
+      center: latLng(0, 0),
+      withPopup: latLng(0, 0),
+      url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+      mapOptions: {
+        zoomSnap: 0.5,
+      },
+      location: "",
+    };
+  },
+  methods: {
+    async setCoordinates(lat, lng, loc) {
+      console.log("PASSED THROUGH: ", lat, lng, loc);
+      this.center = latLng(lat, lng);
+      this.withPopup = latLng(lat, lng);
+      this.location = loc;
+    },
+  },
 };
 </script>
 
